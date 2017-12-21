953c9d71128ac2f9b185a7274c869b210148465d
zukosky <zukosky@sbcglobal.net>
2017-12-21 09:09:01 -0600

    Adding some new control statement

--- controlStatements.py
+++ controlStatements.py
@@ -14,3 +14,18 @@ else:
     outVal = val1 + val2
 
 print("Operation: " + oper + ", Value: " + str(outVal))
+
+# FOR  LOOP
+# By numberic reange
+for nIndex in range(0,15):
+	print(nIndex)
+
+# Breaking off in a loop
+for nIndex in range(0,15):
+	print(nIndex)
+	if nIndex==7:
+		break
+
+#Using xrange instead of range.  Generates the numbers as needed instead of all at once
+for nIndex in xrange(0,12):
+	print(nIndex)
