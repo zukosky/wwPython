522197279c5e752211868a6ba7b8f594bdf40c36
zukosky <zukosky@sbcglobal.net>
2017-12-21 05:48:23 -0600

    Adding a new lists.py.  Added a dictionary within dictionary example

--- dictionaries.py
+++ dictionaries.py
@@ -55,7 +55,8 @@ signalsCopy.clear
 #############################################################################################
 ################################   NESTING   ################################################
 #############################################################################################
-# Example of lists of dictionaries
+# Example of lists of dictionaries. Used when each dictionary contains many kinds of information
+# about the same object.  E.g. everything about one user.  List of dictionary becomes a list of users.
 print("Creating a list of dictionaries...")
 signal1 = {'model':'MD11',
            'year':'2010',
@@ -71,3 +72,28 @@ signalsList = [signal1, signal2, signal3]
 print("Values within the list of dictionaries:")
 for signal in signalsList:
     print(signal.values())
+
+#example of dictionary with lists
+signal4 = {'model':'777',
+           'year':'2016',
+           'signalStrength':'18',
+           'stops':['0RD','PRV','PDX','LGA']
+}
+for values in signal4.values():
+    print(values)
+
+#Example of dictionary within a dictionary
+
+planes = { '777': {
+    'engines':2,
+    'passengers':'350'
+},
+ 'MD11': {
+    'engines':3,
+    'passengers':'300'
+}
+}
+
+for key in planes.keys():
+    print(key)
+    print(planes[key])
---
+++ lists.py
@@ -0,0 +1,3 @@
+# Creating a list.  Note the brackets
+bicycles = ['Trek','Giant','Huffy','Schwinn']
+print(bicycles)
